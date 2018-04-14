e05a8b5477030a121c395037c79af64335519381
LoserAim <38057761+LoserAim@users.noreply.github.com>
2018-04-14 12:38:58 -0700

    created a class

--- app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
+++ app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.andrewdoser.cst238srs01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextWatcher;
-import android.text.Editable;
-import android.widget.*;
-import java.lang.CharSequence;
+
 
 
 
@@ -19,31 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-}
-public abstract class TextValidator implements TextWatcher {
-    private final TextView textView;
 
-    public TextValidator(TextView textView) {
-        this.textView = textView;
-    }
 
-    public abstract void validate(TextView textView, String text);
-
-    @Override
-    final public void afterTextChanged(Editable textView) {
-        String text = textView.getText().toString();
-        validate(textView, text);
-    }
-
-    @Override
-    final public void
-    beforeTextChanged(CharSequence s, int start, int count, int after) {
-        /* Needs to be implemented, but we are not using it. */
-    }
-
-    @Override
-    final public void
-    onTextChanged(CharSequence s, int start, int before, int count) {
-        /* Needs to be implemented, but we are not using it. */
-    }
 }
