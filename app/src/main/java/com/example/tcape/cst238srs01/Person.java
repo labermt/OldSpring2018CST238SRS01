da15e4e2f39dcbcd3118f979a98ca8378614c526
Timothy Capehart <timothy.capehart@oit.edu>
2018-04-15 13:29:19 -0700

    working, test commit

---
+++ app/src/main/java/com/example/tcape/cst238srs01/Person.java
@@ -0,0 +1,13 @@
+package com.example.tcape.cst238srs01;
+
+public class Person {
+    public String Name;
+    public Integer Month;
+    public Integer Day;
+
+    Person(String name, Integer month, Integer day) {
+        this.Name = name;
+        this.Month = month;
+        this.Day = day;
+    }
+}
