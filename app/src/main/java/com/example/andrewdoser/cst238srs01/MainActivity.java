231a8d418d7c79e021849ca0b3f3c4c870328c50
LoserAim <38057761+LoserAim@users.noreply.github.com>
2018-04-17 21:04:45 -0700

    Validation Fields
    Added Field validation code.
    Need to add form validation and persistent data storage.

--- app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
+++ app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
@@ -23,17 +23,73 @@ public class MainActivity extends AppCompatActivity {
         mMM = findViewById(R.id.editTextMonth);
         mDD = findViewById(R.id.editTextDay);
         mButton = findViewById(R.id.buttonSubmit);
+
         mName.addTextChangedListener(new TextValidator(mName) {
             @Override public void validate(TextView textView, String text) {
-                /* Insert your validation rules here */
+                String name = mName.getText().toString();
+                String tname = name.trim();
+                if(tname.isEmpty())
+                {
+                    mName.setError("Please Enter a Name");
+                }
+
             }
         });
+
+        mMM.addTextChangedListener(new TextValidator(mMM) {
+            @Override public void validate(TextView textView, String text) {
+                String name = mMM.getText().toString();
+                String tname = name.trim();
+                if(tname.isEmpty())
+                {
+                    mMM.setError("Please Enter a Month");
+                }
+                else if(!tname.matches("[0-9]+"))
+                {
+                    mMM.setError("Please enter only numbers");
+                }
+                if(tname.matches("[0-9]+"))
+                {
+                    int iname = Integer.parseInt(tname);
+                    if(iname <= 0 || iname > 12)
+                    {
+                        mMM.setError("Please Enter a Valid Month");
+                    }
+                }// iname>=1 && iname<13
+
+            }
+        });
+
+        mDD.addTextChangedListener(new TextValidator(mDD){
+            @Override public void validate(TextView textView, String Text) {
+                String name = mDD.getText().toString();
+                String tname = name.trim();
+
+                if(tname.isEmpty())
+                {
+                    mDD.setError("Please Enter a Day");
+                }
+                else if(!tname.matches("[0-9]+"))
+                {
+                    mDD.setError("Please enter only numbers");
+                }
+                if(tname.matches("[0-9]+"))
+                {
+                    int iname = Integer.parseInt(tname);
+                    if(iname <= 0 || iname >= 31)
+                    {
+                        mDD.setError("Please Enter a Valid Day");
+                    }
+                }
+            }
+        });
+
         mButton.setOnClickListener(
                 new View.OnClickListener()
                 {
                     public void onClick(View view)
                     {
-                        String name = mName.getText().toString();
+
                         String month = mMM.getText().toString();
                         String day = mDD.getText().toString();
                     }
@@ -41,9 +97,22 @@ public class MainActivity extends AppCompatActivity {
         );
 
 
-
-    }
-
+}
+/*   Random random = new Random();
+   int RanNum = random.nextInt(MaxNumber-MinNumber)+MinNumber;
+   String StringEditNum= editTextNum.getText().toString();
+    if(!StringEditNum.equals("")){
+        int EditNum = Integer.parseInt(StringEditNum);
+        if (EditNum >RanNum){
+            textViewStatus.setText("Enter Higher Number");
+        }else if(EditNum <RanNum){
+            textViewStatus.setText("Enter Lower Numer!");
+        }else {
+            textViewStatus.setText("You won! You Nutz!");
+        }
+    } else{
+            Toast.makeText(MainActivity.this,"Try again!",Toast.LENGTH_SHORT).show();
+        }*/
 
 
 
