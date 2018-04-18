a6e4c53ba1b1b2339479fb9d059ac6dbf626a215
LoserAim <38057761+LoserAim@users.noreply.github.com>
2018-04-17 18:58:33 -0700

    Added Some variables
    Create name. month, and day with a button click event. Have not created persistent storage or validation yet.

--- app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
+++ app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
@@ -2,17 +2,46 @@ package com.example.andrewdoser.cst238srs01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mName;
+    EditText mMM;
+    EditText mDD;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mName = findViewById(R.id.editTextName);
+        mMM = findViewById(R.id.editTextMonth);
+        mDD = findViewById(R.id.editTextDay);
+        mButton = findViewById(R.id.buttonSubmit);
+        mName.addTextChangedListener(new TextValidator(mName) {
+            @Override public void validate(TextView textView, String text) {
+                /* Insert your validation rules here */
+            }
+        });
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        String name = mName.getText().toString();
+                        String month = mMM.getText().toString();
+                        String day = mDD.getText().toString();
+                    }
+                }
+        );
+
+
+
     }
 
 
