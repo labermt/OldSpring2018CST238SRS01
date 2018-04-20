296f424af8317280f90eed0f6340fd7c1face212
LoserAim <38057761+LoserAim@users.noreply.github.com>
2018-04-19 17:49:20 -0700

    Added Form Validation

--- app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
+++ app/src/main/java/com/example/andrewdoser/cst238srs01/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.andrewdoser.cst238srs01;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -76,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 if(tname.matches("[0-9]+"))
                 {
                     int iname = Integer.parseInt(tname);
-                    if(iname <= 0 || iname >= 31)
+                    if(iname <= 0 || iname >= 32)
                     {
                         mDD.setError("Please Enter a Valid Day");
                     }
@@ -89,15 +93,100 @@ public class MainActivity extends AppCompatActivity {
                 {
                     public void onClick(View view)
                     {
+                        if(validationSuccess())
+                        {
+                            CharSequence Sub = "Successfully Submitted information";
+                            int duration = Toast.LENGTH_LONG;
+                            Toast Tsub = Toast.makeText(getApplicationContext(), Sub, duration);
+                            TextView TsubMessage = (TextView) Tsub.getView().findViewById(android.R.id.message);
+                            TsubMessage.setTextColor(Color.GREEN);
+                            Tsub.show();
+                        }
 
-                        String month = mMM.getText().toString();
-                        String day = mDD.getText().toString();
+
                     }
                 }
         );
 
 
 }
+
+    private Boolean validationSuccess(){
+        Context context = getApplicationContext();
+       /* if(mName.getText().toString().equalsIgnoreCase("")){
+            Toast.makeText(getApplicationContext(),"Please enter name",0).show();
+            return false;
+        }*/
+        String month = mMM.getText().toString().trim();
+        String day = mDD.getText().toString().trim();
+        CharSequence Feb = "February only has, at most, 29 days. Please change the Day field.";
+        CharSequence Apr = "April only has, at most, 30 days. Please change the Day field.";
+        CharSequence Jun = "June only has, at most, 30 days. Please change the Day field.";
+        CharSequence Nov = "November only has, at most, 30 days. Please change the Day field.";
+        CharSequence Sep = "September only has, at most, 30 days. Please change the Day field.";
+        int duration = Toast.LENGTH_LONG;
+        Toast Tfeb = Toast.makeText(context, Feb, duration);
+        Toast Tapr = Toast.makeText(context, Apr, duration);
+        Toast Tjun = Toast.makeText(context, Jun, duration);
+        Toast Tnov = Toast.makeText(context, Nov, duration);
+        Toast Tsep = Toast.makeText(context, Sep, duration);
+
+        TextView TfebMessage = (TextView) Tfeb.getView().findViewById(android.R.id.message);
+        TfebMessage.setTextColor(Color.RED);
+        TextView TaprMessage = (TextView) Tapr.getView().findViewById(android.R.id.message);
+        TaprMessage.setTextColor(Color.RED);
+        TextView TjunMessage = (TextView) Tjun.getView().findViewById(android.R.id.message);
+        TjunMessage.setTextColor(Color.RED);
+        TextView TnovMessage = (TextView) Tnov.getView().findViewById(android.R.id.message);
+        TnovMessage.setTextColor(Color.RED);
+        TextView TsepMessage = (TextView) Tsep.getView().findViewById(android.R.id.message);
+        TsepMessage.setTextColor(Color.RED);
+            int imonth = Integer.parseInt(month);
+            int iday = Integer.parseInt(day);
+
+            switch(imonth)
+            {
+                case 2:
+                    if(iday > 29)
+                    {
+                        Tfeb.show();
+                        return false;
+                    }
+                    break;
+                case 4:
+                    if(iday > 30)
+                    {
+                        Tapr.show();
+                        return false;
+                    }
+                    break;
+                case 6:
+                    if(iday > 30)
+                    {
+                        Tjun.show();
+                        return false;
+                    }
+                    break;
+                case 9:
+                    if(iday > 30)
+                    {
+                        Tsep.show();
+                        return false;
+                    }
+                    break;
+                case 11:
+                    if(iday > 30)
+                    {
+                        Tnov.show();
+                        return false;
+                    }
+                default:
+                    return true;
+            }
+
+
+        return true;
+    }
 /*   Random random = new Random();
    int RanNum = random.nextInt(MaxNumber-MinNumber)+MinNumber;
    String StringEditNum= editTextNum.getText().toString();
