8ca9b82a22b5e87a90530dd58a55ac4bec68c8bb
Nicholas Slaughter <nicholas.slaughter@oit.edu>
2018-04-21 10:03:41 -0700

    Assignment 2
    Completed version of Happy Birthday 2 You assignment

---
+++ app/src/main/java/com/example/happybirthday2you/people.java
@@ -0,0 +1,16 @@
+package com.example.happybirthday2you;
+
+import android.widget.EditText;
+
+class People {
+    public String _name;
+    public Integer _month;
+    public Integer _day;
+
+    public People (String name, Integer month, Integer day)
+    {
+        _name = name;
+        _month = month;
+        _day = day;
+    }
+}
